64936896d86cd8042f143815a6d6f4b82b263aea
Mahmoud Reyad <m.ryad@more-creatives.com>
2022-11-11 19:54:18 +0200

    feat: 🎸 Navbar Menu Implemention

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import {ContextProvider} from './contexts/contextProvider';
+import {ContextProvider} from './contexts/ContextProvider';
 
 ReactDOM.render(<ContextProvider> <App /> </ContextProvider>, document.getElementById('root'));
